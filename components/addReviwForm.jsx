3fc3373101c9c4a0c5a39347c2fc188e764a2e07
Bahodir8586 <bahodira213@gmail.com>
2021-11-13 11:01:08 +0500

    Add Review finished

--- components/addReviwForm.jsx
+++ components/addReviwForm.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import InputComponent from './inputComponent';
 import TextareaComponent from './textareaComponent';
 import SelectComponent from './selectComponent';
 
@@ -26,7 +25,38 @@ const ratings = [
   },
 ];
 
-export default function EmployeeForm({ submitForm }) {
+const hospitals = [
+  {
+    name: 'Hospital 1',
+    value: 'hos1',
+  },
+  {
+    name: 'Hospital 2',
+    value: 'hos2',
+  },
+  {
+    name: 'Hospital 3',
+    value: 'hos3',
+  },
+];
+const doctors = [
+  {
+    name: 'Doctor 1',
+    value: 'doc1',
+  },
+  {
+    name: 'Doctor 2',
+    value: 'doc2',
+  },
+  {
+    name: 'Doctor 3',
+    value: 'doc3',
+  },
+];
+
+export default function AddReviewForm({ submitForm }) {
+  const [hospital, setHospital] = useState(hospitals[0]);
+  const [doctor, setDoctor] = useState(doctors[0]);
   const [rating, setRating] = useState(ratings[0]);
   const [review, setReview] = useState('');
   return (
@@ -34,7 +64,7 @@ export default function EmployeeForm({ submitForm }) {
       className="max-w-3xl mx-auto space-y-8 divide-y divide-gray-200 border p-8 bg-gray-50 shadow-md rounded-lg"
       onSubmit={(e) => {
         e.preventDefault();
-        submitForm(rating.value, review);
+        submitForm(hospital.value, doctor.value, rating.value, review);
       }}
     >
       <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
@@ -44,6 +74,18 @@ export default function EmployeeForm({ submitForm }) {
           </h3>
 
           <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+            <SelectComponent
+              label={'Hospital'}
+              options={hospitals}
+              value={hospital}
+              onChange={(value) => setHospital(value)}
+            />
+            <SelectComponent
+              label={'Doctor'}
+              options={doctors}
+              value={doctor}
+              onChange={(value) => setDoctor(value)}
+            />
             <SelectComponent
               label={'Role'}
               options={ratings}
@@ -69,7 +111,7 @@ export default function EmployeeForm({ submitForm }) {
             className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             onClick={(e) => {
               e.preventDefault();
-              submitForm(rating.value, review);
+              submitForm(hospital.value, doctor.value, rating.value, review);
             }}
           >
             Submit
